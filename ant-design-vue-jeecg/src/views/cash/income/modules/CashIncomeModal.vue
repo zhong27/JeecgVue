1799658f0236198366af02a85bcc6a30132ebe71
zhong27 <2904013273@qq.com>
2021-02-27 22:53:24 +0800

    资金账户生成

---
+++ ant-design-vue-jeecg/src/views/cash/income/modules/CashIncomeModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <cash-income-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"></cash-income-form>
+  </j-modal>
+</template>
+
+<script>
+
+  import CashIncomeForm from './CashIncomeForm'
+  export default {
+    name: 'CashIncomeModal',
+    components: {
+      CashIncomeForm
+    },
+    data () {
+      return {
+        title:'',
+        width:896,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
