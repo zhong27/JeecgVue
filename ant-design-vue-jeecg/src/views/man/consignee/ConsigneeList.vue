be552fa8c41a32b8ab4e04d78dd443954b5fd2dc
zhong27 <2904013273@qq.com>
2021-03-20 22:18:53 +0800

    添加收货人

---
+++ ant-design-vue-jeecg/src/views/man/consignee/ConsigneeList.vue
@@ -0,0 +1,264 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="创建日期">
+              <j-date :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" placeholder="请选择创建日期" v-model="queryParam.createTime"></j-date>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="姓名">
+              <a-input placeholder="请输入姓名" v-model="queryParam.name"></a-input>
+            </a-form-item>
+          </a-col>
+          <template v-if="toggleSearchStatus">
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <a-form-item label="性别">
+                <j-dict-select-tag placeholder="请选择性别" v-model="queryParam.sex" dictCode="sex"/>
+              </a-form-item>
+            </a-col>
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <a-form-item label="所在地区">
+                <j-area-linkage type="cascader" v-model="queryParam.address" placeholder="请选择省市区"/>
+              </a-form-item>
+            </a-col>
+          </template>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+              <a @click="handleToggleSearch" style="margin-left: 8px">
+                {{ toggleSearchStatus ? '收起' : '展开' }}
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+              </a>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+      <a-button type="primary" icon="download" @click="handleExportXls('收货人信息')">导出</a-button>
+      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+        <a-button type="primary" icon="import">导入</a-button>
+      </a-upload>
+      <!-- 高级查询区域 -->
+      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+      </a-dropdown>
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="pcaSlot" slot-scope="text">
+          <div>{{ getPcaText(text) }}</div>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="downloadFile(text)">
+            下载
+          </a-button>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="handleEdit(record)">编辑</a>
+
+          <a-divider type="vertical" />
+          <a-dropdown>
+            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a @click="handleDetail(record)">详情</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                  <a>删除</a>
+                </a-popconfirm>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </span>
+
+      </a-table>
+    </div>
+
+    <consignee-modal ref="modalForm" @ok="modalFormOk"></consignee-modal>
+  </a-card>
+</template>
+
+<script>
+
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import ConsigneeModal from './modules/ConsigneeModal'
+  import JDictSelectTag from '@/components/dict/JDictSelectTag.vue'
+  import JDate from '@/components/jeecg/JDate.vue'
+  import {filterMultiDictText} from '@/components/dict/JDictSelectUtil'
+  import JAreaLinkage from '@comp/jeecg/JAreaLinkage'
+  import Area from '@/components/_util/Area'
+  import JSuperQuery from '@/components/jeecg/JSuperQuery.vue'
+
+  export default {
+    name: 'ConsigneeList',
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      JDictSelectTag,
+      JDate,
+      JAreaLinkage,
+      ConsigneeModal,
+      JSuperQuery,
+    },
+    data () {
+      return {
+        description: '收货人信息管理页面',
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key:'rowIndex',
+            width:60,
+            align:"center",
+            customRender:function (t,r,index) {
+              return parseInt(index)+1;
+            }
+          },
+          {
+            title:'创建日期',
+            align:"center",
+            dataIndex: 'createTime'
+          },
+          {
+            title:'姓名',
+            align:"center",
+            dataIndex: 'name'
+          },
+          {
+            title:'性别',
+            align:"center",
+            dataIndex: 'sex_dictText'
+          },
+          {
+            title:'年龄',
+            align:"center",
+            dataIndex: 'age'
+          },
+          {
+            title:'电话号码',
+            align:"center",
+            dataIndex: 'phone'
+          },
+          {
+            title:'所在地区',
+            align:"center",
+            dataIndex: 'address',
+            scopedSlots: {customRender: 'pcaSlot'}
+          },
+          {
+            title:'详细地址',
+            align:"center",
+            dataIndex: 'addressDet'
+          },
+          {
+            title:'电子邮箱',
+            align:"center",
+            dataIndex: 'email'
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            fixed:"right",
+            width:147,
+            scopedSlots: { customRender: 'action' }
+          }
+        ],
+        url: {
+          list: "/man/consignee/list",
+          delete: "/man/consignee/delete",
+          deleteBatch: "/man/consignee/deleteBatch",
+          exportXlsUrl: "/man/consignee/exportXls",
+          importExcelUrl: "man/consignee/importExcel",
+          
+        },
+        dictOptions:{},
+        pcaData:'',
+        superFieldList:[],
+      }
+    },
+    created() {
+      this.pcaData = new Area()
+    this.getSuperFieldList();
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      getPcaText(code){
+        return this.pcaData.getText(code);
+      },
+      initDictConfig(){
+      },
+      getSuperFieldList(){
+        let fieldList=[];
+        fieldList.push({type:'datetime',value:'createTime',text:'创建日期'})
+        fieldList.push({type:'string',value:'name',text:'姓名',dictCode:''})
+        fieldList.push({type:'string',value:'sex',text:'性别',dictCode:'sex'})
+        fieldList.push({type:'int',value:'age',text:'年龄',dictCode:''})
+        fieldList.push({type:'string',value:'phone',text:'电话号码',dictCode:''})
+        fieldList.push({type:'pca',value:'address',text:'所在地区'})
+        fieldList.push({type:'string',value:'addressDet',text:'详细地址',dictCode:''})
+        fieldList.push({type:'string',value:'email',text:'电子邮箱',dictCode:''})
+        this.superFieldList = fieldList
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+</style>
---
+++ ant-design-vue-jeecg/src/views/man/consignee/modules/ConsigneeForm.vue
@@ -0,0 +1,223 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form :form="form" slot="detail">
+        <a-row>
+          <a-col :span="24">
+            <a-form-item label="姓名" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['name', validatorRules.name]" placeholder="请输入姓名"  ></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="性别" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <j-dict-select-tag type="list" v-decorator="['sex']" :trigger-change="true" dictCode="sex" placeholder="请选择性别" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="年龄" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input-number v-decorator="['age', validatorRules.age]" placeholder="请输入年龄" style="width: 100%" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="电话号码" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['phone', validatorRules.phone]" placeholder="请输入电话号码"  ></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="所在地区" :labelCol="labelCol" :wrapperCol="wrapperCol">
+             <j-area-linkage type="cascader" v-decorator="['address', validatorRules.address]" placeholder="请输入省市区"  />
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="详细地址" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-textarea v-decorator="['addressDet', validatorRules.addressDet]" rows="4" placeholder="请输入详细地址" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="电子邮箱" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['email', validatorRules.email]" placeholder="请输入电子邮箱"  ></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col v-if="showFlowSubmitButton" :span="24" style="text-align: center">
+            <a-button @click="submitForm">提 交</a-button>
+          </a-col>
+        </a-row>
+      </a-form>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+
+  import { httpAction, getAction } from '@/api/manage'
+  import pick from 'lodash.pick'
+  import { validateDuplicateValue } from '@/utils/util'
+  import JFormContainer from '@/components/jeecg/JFormContainer'
+  import JDictSelectTag from "@/components/dict/JDictSelectTag"
+  import JAreaLinkage from '@comp/jeecg/JAreaLinkage'
+
+  export default {
+    name: 'ConsigneeForm',
+    components: {
+      JFormContainer,
+      JDictSelectTag,
+      JAreaLinkage,
+    },
+    props: {
+      //流程表单data
+      formData: {
+        type: Object,
+        default: ()=>{},
+        required: false
+      },
+      //表单模式：true流程表单 false普通表单
+      formBpm: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    data () {
+      return {
+        form: this.$form.createForm(this),
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+          name: {
+            rules: [
+              { required: true, message: '请输入姓名!'},
+            ]
+          },
+          age: {
+            rules: [
+              { required: false},
+              { pattern: /^-?\d+$/, message: '请输入整数!'},
+            ]
+          },
+          phone: {
+            rules: [
+              { required: true, message: '请输入电话号码!'},
+              { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号码!'},
+            ]
+          },
+          address: {
+            rules: [
+              { required: true, message: '请输入所在地区!'},
+            ]
+          },
+          addressDet: {
+            rules: [
+              { required: true, message: '请输入详细地址!'},
+            ]
+          },
+          email: {
+            rules: [
+              { required: true, message: '请输入电子邮箱!'},
+              { pattern: /^([\w]+\.*)([\w]+)@[\w]+\.\w{3}(\.\w{2}|)$/, message: '请输入正确的电子邮件!'},
+            ]
+          },
+        },
+        url: {
+          add: "/man/consignee/add",
+          edit: "/man/consignee/edit",
+          queryById: "/man/consignee/queryById"
+        }
+      }
+    },
+    computed: {
+      formDisabled(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return false
+          }
+          return true
+        }
+        return this.disabled
+      },
+      showFlowSubmitButton(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return true
+          }
+        }
+        return false
+      }
+    },
+    created () {
+      //如果是流程中表单，则需要加载流程表单data
+      this.showFlowData();
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'createTime','name','sex','age','phone','address','addressDet','email'))
+        })
+      },
+      //渲染流程表单数据
+      showFlowData(){
+        if(this.formBpm === true){
+          let params = {id:this.formData.dataId};
+          getAction(this.url.queryById,params).then((res)=>{
+            if(res.success){
+              this.edit (res.result);
+            }
+          });
+        }
+      },
+      submitForm () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            console.log("表单提交数据",formData)
+            httpAction(httpurl,formData,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+            })
+          }
+         
+        })
+      },
+      popupCallback(row){
+        this.form.setFieldsValue(pick(row,'createTime','name','sex','age','phone','address','addressDet','email'))
+      },
+    }
+  }
+</script>
