adb6497edd9aebd658fa1a27d61ea18739410f1a
zhong27 <2904013273@qq.com>
2021-03-23 11:19:36 +0800

    退货确认

--- ant-design-vue-jeecg/src/views/cash/refund/RefundList.vue
+++ ant-design-vue-jeecg/src/views/cash/refund/RefundList.vue
@@ -6,7 +6,8 @@
         <a-row :gutter="24">
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
             <a-form-item label="客户">
-              <j-search-select-tag placeholder="请选择客户" v-model="queryParam.customer" dict="man_customer,customer_name,id"/>
+              <j-search-select-tag placeholder="请选择客户" v-model="queryParam.customer"
+                                   dict="man_customer,customer_name,id"/>
             </a-form-item>
           </a-col>
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
@@ -43,25 +44,36 @@
 
     <!-- 操作按钮区域 -->
     <div class="table-operator">
-      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+      <a-button @click="refundGoods" type="primary">退货确认</a-button>
+      <a-button @click="refund" type="primary">退款确认</a-button>
       <a-button type="primary" icon="download" @click="handleExportXls('退款管理')">导出</a-button>
-      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+<!--
+      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl"
+                @change="handleImportExcel">
         <a-button type="primary" icon="import">导入</a-button>
       </a-upload>
+-->
       <!-- 高级查询区域 -->
-      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>
+      <j-super-query :fieldList="superFieldList" ref="superQueryModal"
+                     @handleSuperQuery="handleSuperQuery"></j-super-query>
       <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
-          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+          <a-menu-item key="1" @click="batchDel">
+            <a-icon type="delete"/>
+            删除
+          </a-menu-item>
         </a-menu>
-        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+        <a-button style="margin-left: 8px"> 批量操作
+          <a-icon type="down"/>
+        </a-button>
       </a-dropdown>
     </div>
 
     <!-- table区域-begin -->
     <div>
       <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
-        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{
+        selectedRowKeys.length }}</a>项
         <a style="margin-left: 24px" @click="onClearSelected">清空</a>
       </div>
 
@@ -84,7 +96,8 @@
         </template>
         <template slot="imgSlot" slot-scope="text">
           <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
-          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+          <img v-else :src="getImgView(text)" height="25px" alt=""
+               style="max-width:80px;font-size: 12px;font-style: italic;"/>
         </template>
         <template slot="fileSlot" slot-scope="text">
           <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
@@ -102,9 +115,9 @@
         <span slot="action" slot-scope="text, record">
           <a @click="handleEdit(record)">编辑</a>
 
-          <a-divider type="vertical" />
+          <a-divider type="vertical"/>
           <a-dropdown>
-            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
             <a-menu slot="overlay">
               <a-menu-item>
                 <a @click="handleDetail(record)">详情</a>
@@ -132,20 +145,21 @@
   import { JeecgListMixin } from '@/mixins/JeecgListMixin'
   import RefundModal from './modules/RefundModal'
   import JDictSelectTag from '@/components/dict/JDictSelectTag.vue'
-  import {filterMultiDictText} from '@/components/dict/JDictSelectUtil'
+  import { filterMultiDictText } from '@/components/dict/JDictSelectUtil'
   import JSearchSelectTag from '@/components/dict/JSearchSelectTag'
   import JSuperQuery from '@/components/jeecg/JSuperQuery.vue'
+  import { httpAction, getAction } from '@/api/manage'
 
   export default {
     name: 'RefundList',
-    mixins:[JeecgListMixin, mixinDevice],
+    mixins: [JeecgListMixin, mixinDevice],
     components: {
       JDictSelectTag,
       JSearchSelectTag,
       RefundModal,
-      JSuperQuery,
+      JSuperQuery
     },
-    data () {
+    data() {
       return {
         description: '退款管理管理页面',
         // 表头
@@ -153,113 +167,142 @@
           {
             title: '#',
             dataIndex: '',
-            key:'rowIndex',
-            width:60,
-            align:"center",
-            customRender:function (t,r,index) {
-              return parseInt(index)+1;
+            key: 'rowIndex',
+            width: 60,
+            align: 'center',
+            customRender: function(t, r, index) {
+              return parseInt(index) + 1
             }
           },
           {
-            title:'创建日期',
-            align:"center",
+            title: '创建日期',
+            align: 'center',
             dataIndex: 'createTime'
           },
           {
-            title:'退款编号',
-            align:"center",
+            title: '退款编号',
+            align: 'center',
             dataIndex: 'refundNo'
           },
           {
-            title:'客户',
-            align:"center",
+            title: '客户',
+            align: 'center',
             dataIndex: 'customer_dictText'
           },
           {
-            title:'退款金额',
-            align:"center",
+            title: '退款金额',
+            align: 'center',
             dataIndex: 'refundMoney'
           },
           {
-            title:'提单编号',
-            align:"center",
+            title: '提单编号',
+            align: 'center',
             dataIndex: 'billNo'
           },
           {
-            title:'退款类型',
-            align:"center",
+            title: '退款类型',
+            align: 'center',
             dataIndex: 'refundType_dictText'
           },
           {
-            title:'退货确认',
-            align:"center",
+            title: '退货确认',
+            align: 'center',
             dataIndex: 'refundAgree_dictText'
           },
           {
-            title:'退款审核',
-            align:"center",
+            title: '退款审核',
+            align: 'center',
             dataIndex: 'refundStatus_dictText'
           },
           {
-            title:'审核人',
-            align:"center",
+            title: '审核人',
+            align: 'center',
             dataIndex: 'checker'
           },
           {
-            title:'退款时间',
-            align:"center",
+            title: '退款时间',
+            align: 'center',
             dataIndex: 'refundDate'
           },
           {
-            title:'备注',
-            align:"center",
+            title: '备注',
+            align: 'center',
             dataIndex: 'remaks'
           },
           {
             title: '操作',
             dataIndex: 'action',
-            align:"center",
-            fixed:"right",
-            width:147,
+            align: 'center',
+            fixed: 'right',
+            width: 147,
             scopedSlots: { customRender: 'action' }
           }
         ],
         url: {
-          list: "/cash/refund/list",
-          delete: "/cash/refund/delete",
-          deleteBatch: "/cash/refund/deleteBatch",
-          exportXlsUrl: "/cash/refund/exportXls",
-          importExcelUrl: "cash/refund/importExcel",
-          
+          list: '/cash/refund/list',
+          delete: '/cash/refund/delete',
+          deleteBatch: '/cash/refund/deleteBatch',
+          exportXlsUrl: '/cash/refund/exportXls',
+          importExcelUrl: 'cash/refund/importExcel',
+          refundGoods: '/sto/enterHouse/refundGoods'
+
         },
-        dictOptions:{},
-        superFieldList:[],
+        dictOptions: {},
+        superFieldList: []
       }
     },
     created() {
-    this.getSuperFieldList();
+      this.getSuperFieldList()
     },
     computed: {
-      importExcelUrl: function(){
-        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
-      },
+      importExcelUrl: function() {
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+      }
     },
     methods: {
-      initDictConfig(){
+      initDictConfig() {
       },
-      getSuperFieldList(){
-        let fieldList=[];
-        fieldList.push({type:'datetime',value:'createTime',text:'创建日期'})
-        fieldList.push({type:'string',value:'refundNo',text:'退款编号',dictCode:''})
-        fieldList.push({type:'sel_search',value:'customer',text:'客户',dictTable:'man_customer', dictText:'customer_name', dictCode:'id'})
-        fieldList.push({type:'BigDecimal',value:'refundMoney',text:'退款金额',dictCode:''})
-        fieldList.push({type:'string',value:'billNo',text:'提单编号',dictCode:''})
-        fieldList.push({type:'string',value:'refundType',text:'退款类型',dictCode:'refund_type'})
-        fieldList.push({type:'string',value:'refundAgree',text:'退货确认',dictCode:'refund_agree'})
-        fieldList.push({type:'string',value:'refundStatus',text:'退款审核',dictCode:'refund_check'})
-        fieldList.push({type:'string',value:'checker',text:'审核人',dictCode:''})
-        fieldList.push({type:'datetime',value:'refundDate',text:'退款时间'})
-        fieldList.push({type:'string',value:'remaks',text:'备注',dictCode:''})
+      refundGoods() {
+        var len = this.selectedRowKeys.length
+        if (len === 0) {
+          this.$message.warning('请选择至少一条记录！')
+        } else {
+          var ids = ''
+          for (let i = 0; i < len; i++) {
+            ids += this.selectedRowKeys[i] + ','
+          }
+          ids = ids.substring(0,ids.length - 1)
+          var idObj = {id :ids}
+         getAction(this.url.refundGoods,idObj).then((res) => {
+           if (res.success){
+             this.$message.success(res.message)
+           } else{
+             this.$message.error(res.error)
+           }
+         })
+        }
+
+      },
+      getSuperFieldList() {
+        let fieldList = []
+        fieldList.push({ type: 'datetime', value: 'createTime', text: '创建日期' })
+        fieldList.push({ type: 'string', value: 'refundNo', text: '退款编号', dictCode: '' })
+        fieldList.push({
+          type: 'sel_search',
+          value: 'customer',
+          text: '客户',
+          dictTable: 'man_customer',
+          dictText: 'customer_name',
+          dictCode: 'id'
+        })
+        fieldList.push({ type: 'BigDecimal', value: 'refundMoney', text: '退款金额', dictCode: '' })
+        fieldList.push({ type: 'string', value: 'billNo', text: '提单编号', dictCode: '' })
+        fieldList.push({ type: 'string', value: 'refundType', text: '退款类型', dictCode: 'refund_type' })
+        fieldList.push({ type: 'string', value: 'refundAgree', text: '退货确认', dictCode: 'refund_agree' })
+        fieldList.push({ type: 'string', value: 'refundStatus', text: '退款审核', dictCode: 'refund_check' })
+        fieldList.push({ type: 'string', value: 'checker', text: '审核人', dictCode: '' })
+        fieldList.push({ type: 'datetime', value: 'refundDate', text: '退款时间' })
+        fieldList.push({ type: 'string', value: 'remaks', text: '备注', dictCode: '' })
         this.superFieldList = fieldList
       }
     }
